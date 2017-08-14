5fb9f06bf5586e91088966d0f8da2d1aa23b72a4
Andrew Lampinen <lampinen@stanford.edu>
2017-08-13 17:22:15 -0700

    Working model

--- data_splitting.py
+++ data_splitting.py
@@ -1,9 +1,10 @@
 from collections import Counter
+from numpy import random
 
 data_file = "raw_data/ptb.train.txt"
 output_dir = "edited_data/"
 output_file_prefix = "ptb.train."
-
+num_train = 1 #number of lines to use for training
 
 word_counter = Counter()
 with open(data_file, "r") as fin:
@@ -16,14 +17,22 @@ targets = [word for word in word_counter.keys() if word_counter[word] == 20]
 print(targets)
 
 def create_split_for_target_word(word):
-
+    word_lines = []
     with open(data_file, "r") as fin:
 	with open(output_dir + output_file_prefix + "no." + word + '.txt', "w") as noword_f:
-	    with open(output_dir + output_file_prefix + "only." + word + '.txt', "w") as word_f:
 		for line in fin.readlines():
 		    if word in line:
-			word_f.write(line)
+			word_lines.append(line)
 		    else:
 			noword_f.write(line)
+
+    with open(output_dir + output_file_prefix + word + '.wordtrain.txt', "w") as word_train_f:
+	for i in xrange(num_train):
+	    chosen_line = word_lines.pop(random.randint(len(word_lines)))
+	    word_train_f.write(chosen_line)
+    with open(output_dir + output_file_prefix + word + '.wordtest.txt', "w") as word_test_f:
+	word_test_f.writelines(word_lines)
+	
+	
 
 create_split_for_target_word("bonuses")
